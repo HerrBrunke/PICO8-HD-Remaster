aa09177599e45bd1006f35a0c21a3e112521fc54
jannik funke <jannik.f@online.de>
2018-01-30 09:46:41 +0100

    FirstSteps

---
+++ Pico8 HD Remaster/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		    
+	}
+}

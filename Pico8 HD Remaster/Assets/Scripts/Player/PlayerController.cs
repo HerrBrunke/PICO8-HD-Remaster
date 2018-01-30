4fe750a46b1da4af35e4b6e847cad6a3b79e3ab9
jannik funke <jannik.f@online.de>
2018-01-30 10:15:52 +0100

    SecondSteps
    Daniel wo bist du?

--- Pico8 HD Remaster/Assets/Scripts/Player/PlayerController.cs
+++ Pico8 HD Remaster/Assets/Scripts/Player/PlayerController.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-
+    public float velocity;
+    public Rigidbody2D rb;
+    //public Component playertransform = 
 	// Use this for initialization
 	void Start () {
-        speed = 1;
+        velocity = 1f;
+        speed = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		    
-	}
+        float inputHorizontal = Input.GetAxis("Horizonatal");
+        speed = inputHorizontal * velocity * Time.deltaTime;
+
+        PlayerMovement();
+    }
+
+    public void PlayerMovement()
+    {
+        transform.position = new Vector3(speed, 0, 0);
+    }
+    
 }
